57199683e8ed61e87911d3d3193eee46fc869ad0
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-09-14 18:05:31 +0300

    finished add and delete in backend

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 //http://localhost:3001/
 const PORT = process.env.PORT || 3001;
@@ -18,7 +18,7 @@ let BookModel;
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/book");
+  await mongoose.connect(process.env.MONGODB_URL);
 
   const bookSchema = new mongoose.Schema({
     title: String,
@@ -61,21 +61,55 @@ async function addDummyData() {
 }
 app.get("/", homeHandle);
 app.get("/books", bookHandle);
+app.post("/addBooks",addBook);
+app.delete('/deleteBook/:id', deleteBook)
 
 function homeHandle(request, response) {
   response.send("Welcome to Backend Home!!");
 }
 
+function deleteBook(request,response){
+  const id = request.params.id;
+  const email = request.query.email;
+  console.log(id,email);
+  
+  BookModel.deleteOne({_id:id},(err,result)=>{
+    BookModel.find({email: email}, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        response.send(result);
+        console.log('sending data back.....',result);
+      }
+    });
+  })
+}
+
+async function addBook(request,response){
+
+  const {title,description,statis,email} = request.body;
+
+  await BookModel.create({title,description,statis,email})
+
+  BookModel.find({email: email}, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.send(result);
+      console.log('sending data back.....',result);
+    }
+  });
+}
+
 function bookHandle(request, response) {
   const userEmail = request.query.email;
-  BookModel.find({ email: userEmail }, (err, result) => {
+  BookModel.find({email: userEmail}, (err, result) => {
     if (err) {
       console.log(err);
     } else {
       response.send(result);
     }
   });
-
 }
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
